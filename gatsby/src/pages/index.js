897631bdc5f95b7e8c145a185261f4e06045f43e
justinlblc <jleblanc.fra@gmail.com>
2021-06-28 15:57:22 +0200

    Account path set up

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
-import { getUser, isLoggedIn } from "../services/auth"
 
 import Layout from "../components/layout"
 import ArticlesComponent from "../components/articles"
@@ -31,8 +30,6 @@ const IndexPage = () => (
       render={data => (
         <div className="uk-section">
           <div className="uk-container uk-container-large">
-            <h1>Strapi blog: You are logged in, so check your{" "}</h1>
-            <Link to="/profile">profile</Link>
             <ArticlesComponent articles={data.allStrapiArticle.edges} />
           </div>
         </div>
