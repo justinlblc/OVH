52392203bb96279aebf90b91d387daa2da3b0911
justinlblc <jleblanc.fra@gmail.com>
2021-06-28 17:54:24 +0200

    retour en arrière

--- gatsby/src/pages/account.js
+++ gatsby/src/pages/account.js
@@ -1,11 +1,9 @@
 import React from "react"
 
 const Account = () => (
-    <>
   <div>
-    <Link to="https://www.master-7rqtwti-zeguxhectgcvy.ovhcloud-fr-1.webpaas.ovh.net/"></Link>
+    <p>This is going to be a protected route.</p>
   </div>
-    </>
 )
 
 export default Account
