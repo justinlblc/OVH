14b0f88c79a9d433dba8cd5f4b2a22c11ac8d6db
justinlblc <jleblanc.fra@gmail.com>
2021-06-29 14:42:03 +0200

    applying the layout to the home page

--- gatsby/src/pages/account.js
+++ gatsby/src/pages/account.js
@@ -3,6 +3,9 @@ import React from "react"
 const Account = () => (
   <div>
     <p>This is going to be a protected route.</p>
+    <p> You will soon be able to login to you account from there and have 
+        access to the whole content of this website!!
+    </p>
   </div>
 )
 
--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -7,9 +7,16 @@ import ArticlesComponent from "../components/articles"
 import "../assets/css/main.css"
 import {Link} from "gatsby"
 
+import Layout from "../components/layout"
+import ArticlesComponent from "../components/articles"
+
+import "../assets/css/main.css"
+
 export default () => (
+  <Layout>
     <div>
       <p>Hello Gatsby!</p>
       <Link to="/account">Go to your account</Link>
     </div>
+  </Layout>
 )
