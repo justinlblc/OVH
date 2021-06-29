6329eb4e0b8afc89124a54481bd458fcb99a3ca6
justinlblc <jleblanc.fra@gmail.com>
2021-06-29 14:37:10 +0200

    test affichage account

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -9,7 +9,7 @@ import {Link} from "gatsby"
 
 export default () => (
     <div>
-    <p>Hello Gatsby!</p>
-    <Link to="/account">Go to your account</Link>
-  </div>
+      <p>Hello Gatsby!</p>
+      <Link to="/account">Go to your account</Link>
+    </div>
 )
