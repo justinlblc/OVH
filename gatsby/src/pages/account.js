8d351961327f3782f558e5d887aadee60d6fa8f0
justinlblc <jleblanc.fra@gmail.com>
2021-06-29 15:29:24 +0200

    adding link to account page

--- gatsby/src/pages/account.js
+++ gatsby/src/pages/account.js
@@ -1,12 +1,19 @@
 import React from "react"
+import {Link} from "gatsby"
 
 const Account = () => (
+  <>
+  <nav>
+      <Link to="/">Home</Link>{" "}
+      <Link to="/account/category/1">Overview</Link>{" "}
+    </nav>
   <div>
     <p>This is going to be a protected route.</p>
     <p> You will soon be able to login to you account from there and have 
         access to the whole content of this website!!
     </p>
   </div>
+  </>
 )
 
 export default Account
