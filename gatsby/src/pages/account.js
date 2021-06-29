1355b35a6d8d59156fb28d020077b48b75e0259b
justinlblc <jleblanc.fra@gmail.com>
2021-06-29 15:38:01 +0200

    test

--- gatsby/src/pages/account.js
+++ gatsby/src/pages/account.js
@@ -9,9 +9,6 @@ const Account = () => (
     </nav>
   <div>
     <p>This is going to be a protected route.</p>
-    <p> You will soon be able to login to you account from there and have 
-        access to the whole content of this website!!
-    </p>
   </div>
   </>
 )
