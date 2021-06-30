c658efb1ebc05de6e9935fa7e614fc6b8fe6de94
justinlblc <jleblanc.fra@gmail.com>
2021-06-30 15:47:40 +0200

    trying smthg

--- gatsby/src/pages/account.js
+++ gatsby/src/pages/account.js
@@ -1,8 +1,8 @@
 import React from "react"
 import Layout from "../components/layout.js"
 import { Router } from "@reach/router"
-import Profile from "../components/profile"
-import Login from "../components/login"
+import Profile from "../components/profile.js"
+import Login from "../components/login.js"
 
 const Account = () => {
 return (
