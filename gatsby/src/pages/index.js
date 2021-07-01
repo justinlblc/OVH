6357dfecc3dd5852c56a0c220278b7c171eb5eaf
justinlblc <jleblanc.fra@gmail.com>
2021-07-01 10:54:50 +0200

    problem solved for category

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import "../assets/css/main.css"
 
 const IndexPage = () => (
+  <div>
     <p> Page de Garde</p>
+    <Link to="/account"></Link>
+  </div>  
+    
 )
 
 export default IndexPage
