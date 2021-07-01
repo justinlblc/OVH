65c2d8f9407c7e8e9c456f175e2a638e5cf47659
justinlblc <jleblanc.fra@gmail.com>
2021-07-01 10:36:26 +0200

    page account rajoutée

---
+++ gatsby/src/pages/account.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const Account = () => (
+  <Layout>
+      <div> Vous verrez bientôt vos infos perso ici</div>
+  </Layout>
+)
+export default Account
--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,15 +1,9 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
-
-import Layout from "../components/layout"
-import ArticlesComponent from "../components/articles"
 
 import "../assets/css/main.css"
 
 const IndexPage = () => (
-  <Layout>
     <p> Page de Garde</p>
-  </Layout>
 )
 
 export default IndexPage
