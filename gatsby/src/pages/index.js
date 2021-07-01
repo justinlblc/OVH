e51b3d87305c389f2558a3320d5543dd8c7ec854
justinlblc <jleblanc.fra@gmail.com>
2021-07-01 11:02:30 +0200

    all routes issues settled

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,13 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
+import Seo from "./seo"
 
 import "../assets/css/main.css"
 
 const IndexPage = () => (
+  <>
+  <Seo />
   <div>
     <p> Page de Garde</p>
     <Link to="/account">Account</Link>
   </div>  
+  </>
     
 )
 
