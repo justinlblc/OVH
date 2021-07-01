b9e89c3e740742e8ad23d04147e9f0e472313152
justinlblc <jleblanc.fra@gmail.com>
2021-07-01 17:49:32 +0200

    fix for manifest plugin

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -44,6 +44,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
         name: "gatsby-starter-default",
         short_name: "starter",
         start_url: "/",
