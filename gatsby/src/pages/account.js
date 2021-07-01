b2975a9290a847e7d040eeb5297c53e47f56f444
justinlblc <jleblanc.fra@gmail.com>
2021-07-01 11:48:34 +0200

    created account page

---
+++ gatsby/src/pages/account.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Account = () => (
+    <Layout>
+        <p> Welcome to yout account!</p>
+    </Layout>
+)
+
+export default Account
