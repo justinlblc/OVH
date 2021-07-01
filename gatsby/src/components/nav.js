5bf87ba417b368337a91d699e2ebec54bb20cd22
justinlblc <jleblanc.fra@gmail.com>
2021-07-01 13:50:00 +0200

    return to normal

--- gatsby/src/components/nav.js
+++ gatsby/src/components/nav.js
@@ -8,15 +8,7 @@ const Nav = () => (
         <div className="uk-navbar-left">
           <ul className="uk-navbar-nav">
           <li>
-              <Link to="/">Home</Link>
-              {` `}
-          </li>
-          <li>
-              <Link to="/">Profile</Link>
-              {` `}
-          </li>
-          <li>
-              <Link to="/">Logout</Link>
+              <Link to="/">Strapi Blog</Link>
           </li>
           </ul>
         </div>
