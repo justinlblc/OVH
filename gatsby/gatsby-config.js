7ba85e300ecc5386f6658561aaf97ddd6cfc0be9
justinlblc <jleblanc.fra@gmail.com>
2021-07-02 11:26:28 +0200

    adding strapi plugin

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -8,5 +8,13 @@ module.exports = {
     resolve: `gatsby-plugin-create-client-paths`,
     options: { prefixes: [`/account/*`] },
   },
+  {
+    resolve: `gatsby-source-strapi`,
+    options: {
+      apiURL: `https://www.backend.{default}/`,
+      queryLimit: 1000, // Defaults to 100
+      collectionTypes: [`article`, `user`, `category`],
+    },
+  },
 ],
 };
