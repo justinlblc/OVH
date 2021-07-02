23c314609a6b6610fab2cb239c0c8a5840a5da16
justinlblc <jleblanc.fra@gmail.com>
2021-07-02 14:11:41 +0200

    auth0 authentification

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,21 +1,12 @@
 module.exports = {
   siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
     title: "Deal Ex Machina",
-    description: "Deal Ex Machina Website",
-    author: "Deal Ex Machina",
   },
   plugins: ["gatsby-plugin-react-helmet",
   {
     resolve: `gatsby-plugin-create-client-paths`,
     options: { prefixes: [`/account/*`] },
-  },
-  {
-    resolve: `gatsby-source-strapi`,
-    options: {
-      apiURL: `https://www.backend.{default}/`,
-      queryLimit: 1000, // Defaults to 100
-      collectionTypes: [`article`, `user`, `category`],
-    },
   },
 ],
 };
--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const Index = () => (
+export default function Index() {
+  return (
   <div>
     <p>Hello Gatsby!</p>
     <Link to="/account">Go to your account</Link>
   </div>
-)
-
-export default Index
+  )
+}
