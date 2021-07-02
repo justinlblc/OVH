9fcc50bcf09f96b4d38988df51461d05910b5318
justinlblc <jleblanc.fra@gmail.com>
2021-07-02 13:24:50 +0200

    stratin auth0

--- gatsby/src/pages/account.js
+++ gatsby/src/pages/account.js
@@ -1,17 +1,9 @@
 import React from "react"
-import { Router } from "@reach/router"
-import Layout from "../components/layout"
-import PrivateRoute from "../components/privateRoute"
-import Profile from "../components/profile"
-import Login from "../components/login"
 
 const Account = () => (
-  <Layout>
-    <Router>
-      <PrivateRoute path="/account/profile" component={Profile} />
-      <Login path="/account/login" />
-    </Router>
-  </Layout>
+  <div>
+    <p>This is going to be a protected route.</p>
+  </div>
 )
 
 export default Account
--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,26 +1,9 @@
 import React from "react"
-import { getUser, isLoggedIn } from "../services/auth"
-import {Link} from "gatsby"
-import Layout from "../components/layout"
+import { Link } from "gatsby"
 
- export default function Home() {
-  return (
-    <Layout>
-      <h1>Hello {isLoggedIn() ? getUser().name : "world"}!</h1>
-      <p>
-        {isLoggedIn() ? (
-          <>
-            You are logged in, so check your{" "}
-            <Link to="/account/profile">profile</Link>
-            or go to your <Link to="/account">account</Link> to see all the content available 
-          </>
-        ) : (
-          <>
-            You should <Link to="/account/login">log in</Link> to see restricted
-            content
-          </>
-        )}
-      </p>
-    </Layout>
-  )
-}
+export default () => (
+  <div>
+    <p>Hello Gatsby!</p>
+    <Link to="/account">Go to your account</Link>
+  </div>
+)
