6b5fbb7edba811879a59e35aef4cb3b0368df594
justinlblc <jleblanc.fra@gmail.com>
2021-07-05 18:50:12 +0200

    template

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,15 +1,57 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+const config = require("platformsh-config").config();
+
+var backend_route = "";
+if ( config.isValidPlatform() ) {
+  require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+  })
+  backend_route = `http://${config.credentials("strapi")["host"]}`
+} else {
+  require("dotenv").config()
+  backend_route = process.env.API_URL;
+}
+
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: "My super blog",
+    description: "Gatsby blog with Strapi",
+    author: "Strapi team",
+  },
   plugins: [
+    "gatsby-plugin-react-helmet",
     {
-      resolve: `gatsby-plugin-create-client-paths`,
-      options: { prefixes: [`/account/*`] },
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
     },
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: backend_route,
+        contentTypes: [
+          // List of the Content Types you want to be able to request from Gatsby.
+          "article",
+          "category",
+        ],
+        queryLimit: 1000,
+      },
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+      },
+    },
+    "gatsby-plugin-offline",
   ],
 }
