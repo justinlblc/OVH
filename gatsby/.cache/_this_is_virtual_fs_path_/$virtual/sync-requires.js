86450bfcb3af7c78d25c2d16f4368a463ee6372f
justinlblc <jleblanc.fra@gmail.com>
2021-07-05 17:17:24 +0200

    env

---
+++ gatsby/.cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,12 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/home/xyrace/Documents/Stage/OVH/gatsby/.cache/dev-404-page.js")),
+  "component---src-pages-account-js": preferDefault(require("/home/xyrace/Documents/Stage/OVH/gatsby/src/pages/account.js")),
+  "component---src-pages-callback-js": preferDefault(require("/home/xyrace/Documents/Stage/OVH/gatsby/src/pages/callback.js")),
+  "component---src-pages-index-js": preferDefault(require("/home/xyrace/Documents/Stage/OVH/gatsby/src/pages/index.js"))
+}
+
