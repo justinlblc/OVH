df249b5139087872974a9fda2f150b74a2b58946
justinlblc <jleblanc.fra@gmail.com>
2021-07-05 19:15:49 +0200

    auth0

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,42 +1,11 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
+import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import ArticlesComponent from "../components/articles"
-
-import "../assets/css/main.css"
-
-const IndexPage = () => (
-  <Layout>
-    <StaticQuery
-      query={graphql`
-        query {
-          allStrapiArticle {
-            edges {
-              node {
-                strapiId
-                title
-                category {
-                  name
-                }
-                image {
-                  publicURL
-                }
-              }
-            }
-          }
-        }
-      `}
-      render={data => (
-        <div className="uk-section">
-          <div className="uk-container uk-container-large">
-            <h1>Strapi blog</h1>
-            <ArticlesComponent articles={data.allStrapiArticle.edges} />
-          </div>
-        </div>
-      )}
-    />
-  </Layout>
+const Home = () => (
+  <div>
+    <p>Hello Gatsby!</p>
+    <Link to="/account">Go to your account</Link>
+  </div>
 )
 
-export default IndexPage
+export default Home
