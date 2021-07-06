a994d39b0e2e27c6a2a7b577c1436a1a3804c8cf
justinlblc <jleblanc.fra@gmail.com>
2021-07-06 14:10:09 +0200

    few changes

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.master-7rqtwti-zeguxhectgcvy.ovhcloud-fr-1.webpaas.ovh.net/",
     title: "Deal Ex Machina",
   },
   plugins: [
