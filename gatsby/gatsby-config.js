03c40e3ce935dd71202059f9135e128b68a77d6c
justinlblc <jleblanc.fra@gmail.com>
2021-07-07 14:37:04 +0200

    simple site with npm

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,15 +1,7 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [
-    {
-      resolve: `gatsby-plugin-create-client-paths`,
-      options: { prefixes: [`/account/*`] },
-    },
-  ],
-}
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "gatsby",
+  },
+  plugins: [],
+};
