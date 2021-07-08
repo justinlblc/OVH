d62352a38e1c876a3adc2fe62f2c849e57d720b4
justinlblc <jleblanc.fra@gmail.com>
2021-07-08 10:10:43 +0200

    set up of the different pages

---
+++ gatsby/src/pages/home.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Home = () => (
+    <Layout>
+        <p>Here, you will se all the content available on our site.</p>
+    </Layout>
+
+)
+
+export default Home
--- gatsby/src/pages/settings.js
+++ gatsby/src/pages/settings.js
@@ -1,7 +1,13 @@
 import React from "react"
+import Layout from "../components/layout"
 
-const Settings = () => (
-    <p>Settings</p>
-)
+const Settings = ({user}) => {
+    return(
+    <Layout>
+        <p>Hi, {user.name}! Welcome to your account page, where you will be able
+        to see all the information relative to your account.</p>
+    </Layout>
+    )
+}
 
 export default Settings
