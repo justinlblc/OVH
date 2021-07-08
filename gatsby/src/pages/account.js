2f7d00c5b3aaab957f494982297d26e0ba52c0f3
justinlblc <jleblanc.fra@gmail.com>
2021-07-08 17:34:53 +0200

    test

--- gatsby/src/pages/account.js
+++ gatsby/src/pages/account.js
@@ -1,6 +1,9 @@
 import React from "react"
+import { Router } from "@reach/router"
 import { login, isAuthenticated } from "../utils/auth"
 import Layout from "../components/layout"
+import Home from "../components/home"
+import Settings from "../components/settings"
 
 const Account = () => {
     if (!isAuthenticated()) {
@@ -11,9 +14,10 @@ const Account = () => {
     return (
       <>
         <Layout>
-            <div>
-                Welcome to your account page
-            </div>
+        <Router>
+          <Home path="/account/" user={user} />
+          <Settings path="/account/settings" />
+        </Router>
         </Layout>
       </>
     )
