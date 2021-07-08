d1b665f34f3b7c7fc53b7606d4368a55ce1332c5
justinlblc <jleblanc.fra@gmail.com>
2021-07-08 11:02:50 +0200

    link with strapi inside gatsby-config

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
   {
     resolve: `gatsby-source-strapi`,
     options: {
-      apiURL: `http://localhost:1337`,
+      apiURL: backend_route,
       queryLimit: 1000, // Defaults to 100
       collectionTypes: [`article`, `category`],
     },
