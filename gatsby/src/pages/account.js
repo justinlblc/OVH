d4048eebd211fcda2c74a5260b7fad19e0c8872d
justinlblc <jleblanc.fra@gmail.com>
2021-07-08 17:28:27 +0200

    test

--- gatsby/src/pages/account.js
+++ gatsby/src/pages/account.js
@@ -1,12 +1,6 @@
 import React from "react"
-import { login, isAuthenticated, getProfile } from "../utils/auth"
+import { login, isAuthenticated } from "../utils/auth"
 import Layout from "../components/layout"
-import { Router } from "@reach/router"
-
-const Home = ({ user }) => {
-    return <p>Hi, {user.name ? user.name : "friend"}!</p>
-  }
-const Settings = () => <p>Settings</p>
 
 const Account = () => {
     if (!isAuthenticated()) {
@@ -14,15 +8,9 @@ const Account = () => {
       return <p>Redirecting to login...</p>
     }
   
-    const user = getProfile()
-  
     return (
       <>
         <Layout>
-        <Router>
-          <Home path="/account/" user={user} />
-          <Settings path="/account/settings" />
-        </Router>
         </Layout>
       </>
     )
