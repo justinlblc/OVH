52b170af1d905f911468a58932d4d27a332184ac
justinlblc <jleblanc.fra@gmail.com>
2021-07-08 15:05:11 +0200

    index changes

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
 
-export default () => (
+const Index = () => (
   <div>
     <p>Hello Gatsby!</p>
     <Link to="/account">Go to your account</Link>
   </div>
 )
+
+export default Index
