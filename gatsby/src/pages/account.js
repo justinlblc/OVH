8ce07be50ae5eabeb58f1513a09fd36c42f7a083
justinlblc <jleblanc.fra@gmail.com>
2021-07-08 17:49:10 +0200

    test

--- gatsby/src/pages/account.js
+++ gatsby/src/pages/account.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Router } from "@reach/router"
 import { login, isAuthenticated } from "../utils/auth"
-import Nav from "../components/nav"
+import Layout from "../components/layout"
 import Home from "../components/home"
 import Settings from "../components/settings"
 
