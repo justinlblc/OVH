9b85cb47d1d9cd9a256695e7995a478b338697c7
justinlblc <jleblanc.fra@gmail.com>
2021-07-08 17:00:32 +0200

    test

--- gatsby/src/components/layout.js
+++ gatsby/src/components/layout.js
@@ -1,17 +1,14 @@
 import React from "react"
+
 import Nav from "./nav"
-import PropTypes from "prop-types"
 
 const Layout = ({children}) => {
     return (
-        <>
-        <Nav />
-        <main>{children}</main>
-        </>
-    )
+    <>
+    <Nav />
+    <main>{children}</main>
+    </>
+)
 }
-Layout.propTypes = {
-    children: PropTypes.node.isRequired,
-  }
 
 export default Layout
--- gatsby/src/pages/account.js
+++ gatsby/src/pages/account.js
@@ -11,7 +11,7 @@ const Account = () => {
   const user = getProfile()
 
   return (
-    <>
+    <>  
       <Layout>
         <p>Welcome to your account page</p>
       </Layout>
