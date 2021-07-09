63374478b52152e5b4912a67124898dae43164c3
justinlblc <jleblanc.fra@gmail.com>
2021-07-09 10:09:42 +0200

    test

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -7,7 +7,6 @@ module.exports = {
   "gatsby-plugin-react-helmet",
   "gatsby-transformer-sharp",
   "gatsby-plugin-sharp",
-  "gatsby-plugin-offline",
   {
     resolve: `gatsby-source-filesystem`,
     options: {
@@ -19,5 +18,6 @@ module.exports = {
     resolve: `gatsby-plugin-create-client-paths`,
     options: { prefixes: [`/account/*`] },
   },
+  "gatsby-plugin-offline",
 ],
 };
