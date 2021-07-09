160aaee0067d9a621a1431680cb0ff65ff1494d0
justinlblc <jleblanc.fra@gmail.com>
2021-07-09 12:05:10 +0200

    typo

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -2,12 +2,15 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 const Index = ({data}) => {
+  return (  
   <div>
     Hello Gatsby!
     {data.site.siteMetadata.title}
     <Link to="/account">Go to your account throught this link</Link>
   </div>
+  )
 }
+
 export const query = graphql`
 +  query HomePageQuery {
 +    site {
