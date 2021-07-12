113630d9b4bdfbdc07fe305d735eacc2a24f89d5
justinlblc <jleblanc.fra@gmail.com>
2021-07-12 10:17:21 +0200

    query in index simple

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -4,16 +4,16 @@ import { Link, graphql } from "gatsby"
 const Index = ({data}) => {
   return (  
   <div>
-    {data.site.siteMetadata.description}
+    <p>{data.site.siteMetadata.description}</p>
     <Link to="/account">Go to your account throught this link</Link>
   </div>
   )
 }
 export const query = graphql`
-query IndexQuery {
-  site {
-    siteMetadata {
-      title
+  query IndexQuery {
+   site {
+      siteMetadata {
+       title
     }
   }
 }
