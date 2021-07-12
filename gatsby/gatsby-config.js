c8b0ce8ac73c65b011adef010790909bce2dd724
justinlblc <jleblanc.fra@gmail.com>
2021-07-12 17:28:29 +0200

    test

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,16 +1,3 @@
-const config = require("platformsh-config").config();
-
-var backend_route = "";
-if ( config.isValidPlatform() ) {
-  require("dotenv").config({
-    path: `.env.${process.env.NODE_ENV}`,
-  })
-  backend_route = `http://${config.credentials("strapi")["host"]}`
-} else {
-  require("dotenv").config()
-  backend_route = process.env.API_URL;
-}
-
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
@@ -28,7 +15,7 @@ module.exports = {
   {
     resolve: "gatsby-source-strapi",
     options: {
-      apiURL: backend_route,
+      apiURL: process.env.API_URL,
       contentTypes: [
         "article",
         "category",
