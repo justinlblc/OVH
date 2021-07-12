d686593a9d07a03af523bfd1211305d156e87928
justinlblc <jleblanc.fra@gmail.com>
2021-07-12 15:46:46 +0200

    query in inde page with strapi

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,10 +1,32 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, StaticQuery, graphql } from "gatsby"
+
+const query = graphql`
+  query {
+    allStrapiArticle {
+      edges {
+        node {
+          strapiId
+          title
+        }
+      }
+    }
+  }
+`
 
 const Index = () => {
   return (  
   <div>
-  
+    <StaticQuery
+    query={query}
+    render={data => (
+      <ul>
+        {data.allStrapiArticle.edges.map(article => (
+          <li key={article.node.strapiID}>{article.node.title}</li>
+        ))}
+      </ul>
+    )}
+    />
     <Link to="/account">Go to your account there</Link>
   </div>
   )
