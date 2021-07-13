b78defbaec4ee588b03615e3b0989379d6493366
justinlblc <jleblanc.fra@gmail.com>
2021-07-13 09:51:14 +0200

    template

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -3,34 +3,5 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "gatsby",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud",
-  "gatsby-plugin-react-helmet",
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `images`,
-      path: `${__dirname}/src/images`,
-    },
-  },
-  {
-    resolve: "gatsby-source-strapi",
-    options: {
-      apiURL: process.env.API_URL,
-      contentTypes: [
-        "article",
-        "category",
-      ],
-      queryLimit: 1000,
-    },
-  },
- 
-  "gatsby-transformer-sharp",
-  "gatsby-plugin-sharp",
-  {
-    resolve: `gatsby-plugin-create-client-paths`,
-    options: { prefixes: [`/account/*`] },
-  },
-  "gatsby-plugin-offline",
-  `gatsby-plugin-playground`,
-],
+  plugins: [],
 };
