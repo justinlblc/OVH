f62565ff92a60a5181eb0d1f9387b557debc36a3
justinlblc <jleblanc.fra@gmail.com>
2021-07-13 10:47:42 +0200

    no query

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,47 +1,9 @@
 import React from "react"
 import { Link, StaticQuery, graphql } from "gatsby"
 
-const query = graphql`
-  query {
-    allStrapiArticle {
-      edges {
-        node {
-          strapiId
-          title
-        }
-      }
-    }
-  }
-`
-
 const Index = () => {
   return (  
   <div>
-    <StaticQuery
-              query={graphql`
-                query {
-                  allStrapiCategory {
-                    edges {
-                      node {
-                        strapiId
-                        name
-                      }
-                    }
-                  }
-                }
-              `}
-              render={data =>
-                data.allStrapiCategory.edges.map((category, i) => {
-                  return (
-                    <li key={category.node.strapiId}>
-                      <Link to={`/category/${category.node.strapiId}`}>
-                        {category.node.name}
-                      </Link>
-                    </li>
-                  )
-                })
-              }
-            />
     <Link to="/account">Go to your account there</Link>
   </div>
   )
