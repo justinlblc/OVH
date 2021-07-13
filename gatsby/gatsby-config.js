fdf72ee7271313c6191f99ca3df9bab43f03238f
justinlblc <jleblanc.fra@gmail.com>
2021-07-13 14:53:15 +0200

    trying gatsby-strapi backend_route

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,3 +1,15 @@
+const config = require("platformsh-config").config();
+
+var backend_route = "";
+if ( config.isValidPlatform() ) {
+  require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+  })
+  backend_route = `http://${config.credentials("strapi")["host"]}`
+} else {
+  require("dotenv").config()
+  backend_route = process.env.API_URL;
+}
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
@@ -15,7 +27,7 @@ module.exports = {
   {
     resolve: "gatsby-source-strapi",
     options: {
-      apiURL: "https://backend.master-7rqtwti-zeguxhectgcvy.ovhcloud-fr-1.webpaas.ovh.net",
+      apiURL: backend_route,
       contentTypes: [
         "article",
         "category",
