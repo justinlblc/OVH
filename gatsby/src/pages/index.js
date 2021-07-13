5d5a2527f3c430ba80c7f9b347d5e65810ba3ed4
justinlblc <jleblanc.fra@gmail.com>
2021-07-13 14:27:23 +0200

    added plugin to strapi and testing simple site query

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,13 +1,26 @@
 import React from "react"
 import { Link, StaticQuery, graphql } from "gatsby"
 
-const Index = () => {
+const Index = ({data}) => {
+  const {title} = data.site.siteMetadata
   return (  
   <div>
-    <Link to="/account">Go to your account there</Link>
+    <Link to="/account">Go to your account there ({title)}</Link>
   </div>
   )
 }
+
+export const query = graphql`
+    query IndexTest {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+`
+
+
 
 export default Index
 
