118d1b17332ee9ba5b54489bd48f00bf22ddfc8a
justinlblc <jleblanc.fra@gmail.com>
2021-07-13 17:03:45 +0200

    test

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,38 +1,36 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "gatsby",
+    siteUrl: `https://www.yourdomain.tld`,
+    title: `gatsby`,
   },
-  plugins: ["gatsby-plugin-gatsby-cloud",
-  "gatsby-plugin-react-helmet",
+  plugins: [`gatsby-plugin-gatsby-cloud`,
+  `gatsby-plugin-react-helmet`,
   {
     resolve: `gatsby-source-filesystem`,
     options: {
       name: `images`,
-      path: `./src/images`,
+      path: `${__dirname}/src/images`,
     },
   },
   {
-    resolve: "gatsby-source-strapi",
+    resolve: `gatsby-source-strapi`,
     options: {
-      apiURL: "http://localhost:1337",
+      apiURL: `http://localhost1337`,
       contentTypes: [
-        "article",
-        "category",
+        `article`,
+        `category`,
       ],
       queryLimit: 1000,
     },
   },
  
-  "gatsby-transformer-sharp",
-  "gatsby-plugin-sharp",
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-sharp`,
   {
     resolve: `gatsby-plugin-create-client-paths`,
     options: { prefixes: [`/account/*`] },
   },
-  "gatsby-plugin-offline",
+  `gatsby-plugin-offline`,
   `gatsby-plugin-playground`,
 ],
-
 };
-
