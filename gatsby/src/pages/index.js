69e0d39f79e50971429e01a15208a51e2d88f7e4
justinlblc <jleblanc.fra@gmail.com>
2021-07-16 00:28:10 +0200

    trying out strapi query

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,7 +1,22 @@
 import React from "react"
+import {graphql} from "gatsby"
 
-const Index = () => (
-  <div>Welcome to a site which is FINALLY working (at least I hope so) </div>
-)
+const Index = ({data}) => {
+  return (
+  <div>Welcome to a site which is FINALLY working (at least I hope so) {data.allStrapiArticle.edges} </div>
+  )
+}
+
+export const quey = graphql`
+  query Index {
+    allStrapiArticle {
+      edges {
+        node {
+          title
+        }
+      }
+    }
+  }
+`
 
 export default Index
