13ddaa99bad8c99548ba95947f5ebfc6e64ef3dd
justinlblc <jleblanc.fra@gmail.com>
2021-07-15 13:44:43 +0200

    test

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 const Index = () => {
   return (  
   <div>
-    <Link to="/account">Go to your account there</Link>
+    <Link to="/account">Go to your account there  --</Link>
   </div>
   )
 }
