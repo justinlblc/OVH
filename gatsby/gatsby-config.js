52919921f86fa608faa0b6b4d44424fc4015f084
justinlblc <jleblanc.fra@gmail.com>
2021-07-16 00:01:14 +0200

    trying new url

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://0.0.0.0:1337`,
+        apiURL: `http://backend.master-7rqtwti-zeguxhectgcvy.ovhcloud-fr-1.webpaas.ovh.net`,
         queryLimit: 1000, // Defaults to 100
         collectionTypes: [`article`],
       },
