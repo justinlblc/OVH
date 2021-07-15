92f152065a371c7fa63ae8c2afe4bfea118bc9f3
justinlblc <jleblanc.fra@gmail.com>
2021-07-16 00:31:47 +0200

    deleting query

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,22 +1,9 @@
 import React from "react"
 import {graphql} from "gatsby"
 
-const Index = ({data}) => {
+const Index = () => {
   return (
-  <div>Welcome to a site which is FINALLY working (at least I hope so) {data.allStrapiArticle.edges} </div>
+  <div>Welcome to a site which is FINALLY working (at least I hope so) </div>
   )
 }
-
-export const quey = graphql`
-  query Index {
-    allStrapiArticle {
-      edges {
-        node {
-          title
-        }
-      }
-    }
-  }
-`
-
 export default Index
