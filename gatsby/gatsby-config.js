e6380663036ec3cf865bbdfb2bff23789dc56347
justinlblc <jleblanc.fra@gmail.com>
2021-07-16 00:04:21 +0200

    test

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,3 +1,16 @@
+const config = require("platformsh-config").config();
+
+var backend_route = "";
+if ( config.isValidPlatform() ) {
+  require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+  })
+  backend_route = `http://${config.credentials("strapi")["host"]}`
+} else {
+  require("dotenv").config()
+  backend_route = process.env.API_URL;
+}
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
@@ -7,7 +20,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://backend.master-7rqtwti-zeguxhectgcvy.ovhcloud-fr-1.webpaas.ovh.net`,
+        apiURL: backend_route,
         queryLimit: 1000, // Defaults to 100
         collectionTypes: [`article`],
       },
