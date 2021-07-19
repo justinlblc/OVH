4bcb750b48c96aa5b6fa8625ec6d1dbba2943594
justinlblc <jleblanc.fra@gmail.com>
2021-07-19 17:29:06 +0200

    creating article pages

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -19,3 +19,38 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
       })
     }
   }
+
+  exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions
+    const result = await graphql(
+      `
+        {
+          articles: allStrapiArticle {
+            edges {
+              node {
+                title
+              }
+            }
+          }
+      `
+    )
+  
+    if (result.errors) {
+      throw result.errors
+    }
+  
+    // Create blog articles pages.
+    const articles = result.data.articles.edges
+    const categories = result.data.categories.edges
+  
+    articles.forEach((article, index) => {
+      createPage({
+        path: `/account/article/${article.node.title}`,
+        component: require.resolve("./src/templates/article.js"),
+        context: {
+          id: article.node.strapiId,
+        },
+      })
+    })
+  }
+  
---
+++ gatsby/src/templates/article.js
@@ -0,0 +1,49 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Layout from "../components/layout"
+import Img from "gatsby-image"
+
+const Article = () => {
+    const query=graphql`
+    query ($id: Int!){
+        strapiArticle(id: {eq: "$id"}) {
+    strapiId
+    content
+    publishedAt
+    title
+    picture {
+      localFile {
+        childImageSharp {
+          fixed(height: 125, width: 200)
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+}
+`
+return (
+    <Layout>
+    <StaticQuery
+    query={query}
+    render={data => (
+        <ul>
+            <header>
+                <h1>{data.strapiArticle.title}</h1>
+            </header>
+            <div>
+                <div>
+                    <Img fixed={strapiArticle.picture.localfile.childImageSharp.fixed}/>
+                </div>
+                <div>
+                <p>{data.strapiArticle.content} </p>
+                <p>{data.strapiArticle.publishedAt}</p>
+                </div>
+            </div>
+        </ul>
+    )}
+    />
+     </Layout>
+)
+}
+export default Article
