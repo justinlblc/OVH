c796fbaa57987a00f810d61d1b408da24ac377a3
justinlblc <jleblanc.fra@gmail.com>
2021-07-20 11:54:16 +0200

    index as root

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,41 +1,18 @@
 import React from "react"
-import {StaticQuery, graphql, Link} from "gatsby"
-import Img from "gatsby-image"
 import {isAuthenticated } from "../utils/auth"
 import Layout from "../components/layout"
 import Home from "../components/home"
 import Settings from "../components/settings"
 import Account from "../components/account"
 
-const query=graphql`
-  query {
-  allStrapiHome {
-    edges {
-      node {
-        strapiId
-        title
-        content
-        picture {
-          localFile {
-            childImageSharp {
-              fixed (height: 410, width: 1200){
-                ...GatsbyImageSharpFixed
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
-`
-
 const Index = () => {
   if (!isAuthenticated()) {
     return (
-  <Home>
-
-  </Home>
+      <>
+  <Layout>
+    <Home />
+  </Layout>
+  </>
     )}
 return (
   <>
